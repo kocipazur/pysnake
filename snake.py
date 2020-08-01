70399a4a439aa5ac5abe8af5c6e2b1caf54d8c2f
kocipazur <naturat.am@gmail.com>
2020-08-01 23:40:50 +0200

    Refactor

--- snake.py
+++ snake.py
@@ -2,15 +2,33 @@ import os
 import random
 import ctypes
 import time
-kernel32 = ctypes.WinDLL('kernel32')
-hStdOut = kernel32.GetStdHandle(-11)
-mode = ctypes.c_ulong()
-kernel32.GetConsoleMode(hStdOut, ctypes.byref(mode))
-mode.value |= 4
-kernel32.SetConsoleMode(hStdOut, mode)
+
+
+# <editor-fold desc="Definitions">
+# region Definitions
+
+def initialize_windows_shell():
+    kernel32 = ctypes.WinDLL('kernel32')
+    hStdOut = kernel32.GetStdHandle(-11)
+    mode = ctypes.c_ulong()
+    kernel32.GetConsoleMode(hStdOut, ctypes.byref(mode))
+    mode.value |= 4
+    kernel32.SetConsoleMode(hStdOut, mode)
 
 
 def clear(): return os.system('cls')
+
+
+class bcolors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+
+
+def colored_text(text, color):
+    END = '\033[0m'
+    return f"{color}{text}{END}"
 
 
 def print_game(game_size, snake, snack):
@@ -28,6 +46,19 @@ def print_game(game_size, snake, snack):
         print()
 
 
+def show_title():
+    print("pySnake!")
+
+
+def show_controlls():
+    print("Controls:")
+    print("w - up")
+    print("s - down")
+    print("a - left")
+    print("d - right")
+    print("enter - confirm move")
+
+
 def remove_last_element(array):
     array.pop(len(array) - 1)
 
@@ -42,15 +73,16 @@ def move_snake(body, position, fed):
         remove_last_element(body)
 
 
-def map_input_to_next_position(current_position, input):
+def map_input_to_next_position(input, current_position):
+    inputed = input()
     b = (0, 0)
-    if (input == "w"):
+    if (inputed == "w"):
         b = (0, -1)
-    if (input == "s"):
+    if (inputed == "s"):
         b = (0, 1)
-    if (input == "a"):
+    if (inputed == "a"):
         b = (-1, 0)
-    if (input == "d"):
+    if (inputed == "d"):
         b = (1, 0)
     return (current_position[0] + b[0], current_position[1] + b[1])
 
@@ -62,65 +94,87 @@ def validate_Step(size, body, step):
     return valid
 
 
-def colored_text(text, color):
-    END = '\033[0m'
-    return f"{color}{text}{END}"
-
-
-class bcolors:
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-
-
-size = 9
-is_game_running = True
-is_fed = True
-initial_length = 3
-starting_position = (4, 4)
-snack_position = (None, None)
-snake_body = [starting_position]
-next_step = starting_position
-validStep = False
-snack_needed = False
-
-print("pySnake!")
-print("Controls:")
-print("w - up")
-print("s - down")
-print("a - left")
-print("d - right")
-print("enter - confirm move")
-print("Press enter to start!")
-input()
-
-while (is_game_running):
-    clear()
+def show_score(snake_body, initial_length):
     print(
-        f"Score:  {colored_text(len(snake_body) - 3, bcolors.BLUE) if len(snake_body) - 3 >= 0 else 0}")
-    print_game(size, snake_body, snack_position)
-    is_fed = False
+        f"Score: {colored_text(len(snake_body) - initial_length, bcolors.BLUE) if len(snake_body) - initial_length >= 0 else colored_text(0, bcolors.BLUE)}")
+
+
+def get_keyboard_input():
+    user_input = input()
+    return user_input
+
+
+def get_next_step(next_step, size, snake_body, input_function):
+    validStep = False
     while (not validStep):
         current_step = next_step
-        next_step = map_input_to_next_position(next_step, input())
-        #next_step = map_input_to_next_position(next_step, random.choice(["w", "s", "a", "d"]))
-        # time.sleep(1)
+        next_step = map_input_to_next_position(input_function, next_step)
         validStep = validate_Step(size, snake_body, next_step)
         if (not validStep):
             next_step = current_step
-    if (next_step == snack_position):
-        is_fed = True
-        snack_needed = True
-    if (initial_length > 1):
-        initial_length -= 1
-        if (initial_length == 1):
-            snack_needed = True
-        is_fed = True
-    move_snake(snake_body, next_step, is_fed)
-    if (snack_needed):
-        while (snack_position in snake_body or (snack_needed)):
-            snack_position = (random.randint(0, size - 1),
-                              random.randint(0, size - 1))
+    return next_step
+
+
+def generate_snack_position(snake_body, size):
+    snack_needed = True
+    snack_position = (None, None)
+    while (snack_position in snake_body or (snack_needed)):
+        snack_position = (random.randint(0, size - 1),
+                          random.randint(0, size - 1))
+        snack_needed = False
+    return snack_position
+
+
+def run_initial_game_loop(snake_body, initial_length, starting_position, size, inputMethod):
+    is_fed = True
+    next_step = starting_position
+    for i in range(0, initial_length - 1):
+        clear()
+        show_score(snake_body, initial_length)
+        print_game(size, snake_body, None)
+        next_step = get_next_step(
+            next_step, size, snake_body, inputMethod)
+        move_snake(snake_body, next_step, is_fed)
+    return snake_body
+
+
+def run_main_game_loop(snake_body, initial_length, size, inputMethod):
+    is_fed = False
+    snack_needed = True
+    next_step = snake_body[0]
+    is_game_running = True
+    while (is_game_running):
+        clear()
+        show_score(snake_body, initial_length)
+        if (snack_needed):
+            snack_position = generate_snack_position(snake_body, size)
             snack_needed = False
-    validStep = False
+        print_game(size, snake_body, snack_position)
+        next_step = get_next_step(
+            next_step, size, snake_body, inputMethod)
+        if (next_step == snack_position):
+            is_fed = True
+            snack_needed = True
+        move_snake(snake_body, next_step, is_fed)
+        is_fed = False
+
+
+def run_game():
+    size = 9
+    initial_length = 3
+    starting_position = (4, 4)
+    snake_body = [starting_position]
+    show_title()
+    show_controlls()
+    input()
+    snake_body = run_initial_game_loop(
+        snake_body, initial_length, starting_position, size, get_keyboard_input)
+    run_main_game_loop(snake_body, initial_length, size, get_keyboard_input)
+
+# endregion
+# </editor-fold>
+# ================================================= end of definitions
+
+
+initialize_windows_shell()
+run_game()
